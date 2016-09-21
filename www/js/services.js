d0fa35e095444cb7d9da9c55bd563f70949fe787
leadpython <jeffrey.campecino@gmail.com>
2016-09-21 14:17:14 -0700

    target list created

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,162 @@
 angular.module('lilbro.services', [])
 
 .factory('TargetSERVICES', function() {
+  var targetServices = {};
 
+  targetServices.targets = [
+    {
+      type: 'Credit Cards',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    },
+
+    {
+      type: 'Debit Cards',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    },
+    
+    {
+      type: 'Local Business',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    },
+    
+    {
+      type: 'Casinos',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    },
+    
+    {
+      type: 'Drug Cartels',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    },
+    
+    {
+      type: 'Covert Agencies',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    },
+    
+    {
+      type: 'Multinational Corporations',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    },
+    
+    {
+      type: 'World Bank',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    },
+    
+    {
+      type: 'Black Hat Hackers',
+      security: {
+        passLength: 0,
+        timeLimit: undefined,
+        tries: 10
+      },
+      reward: {
+        min: 0,
+        max: 0
+      },
+      cost: {
+        fee: 0,
+        penalty: 0
+      }
+    }
+  ];
+
+  return targetServices;
 })
